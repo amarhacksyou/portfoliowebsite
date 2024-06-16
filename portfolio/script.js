3e2de0c098a11dbd42f1e7749fe4a3a9ad9f904b
Amar Kumar Saw <162687196+amarhacksyou@users.noreply.github.com>
2024-06-16 15:32:34 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+    const items = document.querySelectorAll(".item");
+    e.target.matches(".next") && slider.append(items[0]);
+    // e.target.matches(".prev") && slider.append(items[items-1]);
+}
+
+document.addEventListener("click",activate,false);
+
